bd19b2561f1e5cc1d8ed051ad98900aeaaf7d285
Piero Jeanpier Araujo Rodriguez <paraujor1@upao.edu.pe>
2026-09-23 08:32:29 -0500

    feat: Se crea en frontend un index.html además del DOCKERFILE y una configuración default para NGINX

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Tarea de laboratorio semana 3 - Terraform Hands-on Lab
-# laboratorio-semana-3---Terraform-Hands-on-Lab
---
+++ iac/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "4.6.0"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
